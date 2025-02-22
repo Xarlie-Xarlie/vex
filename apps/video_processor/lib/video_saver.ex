4e43b004c0e46b90df8bda0874296b775abbece4
charliecharlie <jeancharles552@gmail.com>
2025-02-22 12:04:43 -0300

    fix: saver genserver functions

--- apps/video_processor/lib/video_saver.ex
+++ apps/video_processor/lib/video_saver.ex
@@ -9,16 +9,31 @@ defmodule VideoProcessor.Saver do
   def init(state), do: {:ok, state}
 
   @impl true
+  def handle_call(:log, from, state) do
+    GenServer.reply(from, state)
+    {:reply, [], state}
+  end
+
+  @impl true
   def handle_cast({:remove_processed_file, file_name}, state) do
+    File.close(file_name)
+
     Map.drop(state, [file_name])
     |> then(&{:noreply, &1})
   end
 
   @impl true
   def handle_cast({:assign_file, %{file_name: file_name, total_chunks: total_chunks}}, state) do
-    # File.open!(file_name)
+    File.rm(file_name)
+    file = File.open!(file_name, [:binary, :append])
+
     {:noreply,
-     Map.put(state, file_name, %{total_chunks: total_chunks, next_chunk: 1, buffer: %{}})}
+     Map.put(state, file_name, %{
+       total_chunks: total_chunks,
+       next_chunk: 1,
+       buffer: %{},
+       file: file
+     })}
   end
 
   @impl true
@@ -30,79 +45,46 @@ defmodule VideoProcessor.Saver do
       nil ->
         state
 
-      %{total_chunks: ^current_chunk, next_chunk: ^current_chunk, buffer: %{}} ->
-        IO.binwrite(chunk)
-        File.close(file_name)
+      %{total_chunks: ^current_chunk, next_chunk: ^current_chunk, buffer: %{}, file: file} ->
+        IO.binwrite(file, chunk)
+        File.close(file)
         Map.drop(state, [file_name])
 
-      %{next_chunk: ^current_chunk} ->
-        save_chunk(chunk, file_name, state)
+      %{next_chunk: ^current_chunk, file: file} ->
+        IO.binwrite(file, chunk)
+        Map.update!(state, file_name, &save_buffered(&1, file_name))
 
       %{next_chunk: next_chunk} when next_chunk !== current_chunk ->
         Map.update!(
           state,
           file_name,
-          fn %{total_chunks: total_chunks, next_chunk: next_chunk, buffer: buffer} ->
-            %{
-              total_chunks: total_chunks,
-              next_chunk: next_chunk,
-              buffer: Map.put(buffer, current_chunk, chunk)
-            }
+          fn file_info ->
+            %{file_info | buffer: Map.put(file_info.buffer, current_chunk, chunk)}
           end
         )
     end
     |> then(&{:noreply, &1})
   end
 
-  defp save_chunk(chunk, file_name, state) do
-    IO.binwrite(file_name, chunk)
-    Map.update!(state, file_name, &save_buffered(&1, file_name, chunk))
-  end
-
   defp save_buffered(
          %{total_chunks: total_chunks, next_chunk: next_chunk, buffer: %{}},
-         file_name,
-         nil
+         file_name
        )
        when next_chunk > total_chunks do
     GenServer.cast(__MODULE__, {:remove_processed_file, file_name})
   end
 
-  defp save_buffered(
-         %{total_chunks: total_chunks, next_chunk: next_chunk, buffer: buffer},
-         file_name,
-         nil
-       ) do
+  defp save_buffered(%{next_chunk: next_chunk, buffer: buffer} = file_info, file_name) do
     case Map.get(buffer, next_chunk) do
       nil ->
-        buffer
-        |> then(&%{total_chunks: total_chunks, next_chunk: next_chunk, buffer: &1})
+        %{file_info | next_chunk: next_chunk + 1}
 
       data ->
         IO.binwrite(file_name, data)
 
         Map.drop(buffer, [next_chunk])
-        |> then(&%{total_chunks: total_chunks, next_chunk: next_chunk + 1, buffer: &1})
-        |> save_buffered(file_name, nil)
+        |> then(&%{file_info | next_chunk: next_chunk + 1, buffer: &1})
+        |> save_buffered(file_name)
     end
-  end
-
-  defp save_buffered(
-         %{total_chunks: total_chunks, next_chunk: next_chunk, buffer: buffer},
-         file_name,
-         chunk
-       ) do
-    case Map.get(buffer, next_chunk + 1) do
-      nil ->
-        Map.put(buffer, next_chunk + 1, chunk)
-        |> then(&%{total_chunks: total_chunks, next_chunk: next_chunk + 1, buffer: &1})
-
-      data ->
-        IO.binwrite(file_name, data)
-
-        Map.drop(buffer, [next_chunk + 1])
-        |> then(&%{total_chunks: total_chunks, next_chunk: next_chunk + 2, buffer: &1})
-    end
-    |> save_buffered(file_name, nil)
   end
 end
